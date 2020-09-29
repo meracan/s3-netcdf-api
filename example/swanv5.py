6f36fe7fe03b707b0192a3447cd1492177d7f376
Julien.Cousineau <julien.cousineau@gmail.com>
2020-09-29 12:28:07 +0000

    Added plot example

--- example/swanv5.py
+++ example/swanv5.py
@@ -1,7 +1,22 @@
 import pandas as pd
-import plotly.graph_objects as go
 
-df = pd.read_csv("https://w4wj62194j.execute-api.us-west-2.amazonaws.com/Prod?variable=hs&inode=0&export=csv")
-fig = go.Figure([go.Scatter(x=df['Datetime'], y=df['hs,m'])])
-fig.write_image("hs_0.png")
 
+
+
+def matplotlib_example():
+    import matplotlib.pyplot as plt
+    df = pd.read_csv("https://w4wj62194j.execute-api.us-west-2.amazonaws.com/Prod?variable=hs&inode=0&export=csv")
+    plt.plot_date(df['Datetime'].astype("datetime64[h]"),df['hs,m'],"-")
+    plt.savefig("hs_0.matplotlib.png")
+
+def plotly_example():
+    import plotly.graph_objects as go
+    df = pd.read_csv("https://w4wj62194j.execute-api.us-west-2.amazonaws.com/Prod?variable=hs&inode=0&export=csv")
+    fig = go.Figure([go.Scatter(x=df['Datetime'], y=df['hs,m'])])
+    fig.write_image("hs_0.plotly.png")
+    
+    
+if __name__ == "__main__":
+  matplotlib_example()
+  plotly_example()
+      
